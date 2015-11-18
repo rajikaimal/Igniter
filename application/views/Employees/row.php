ab101734a5d0210c10dc26102a79d23560fdf6d1
Rajika <inbox.rajika@gmail.com>
2015-11-18 09:32:12 +0530

    Anchor to income

--- application/views/Employees/row.php
+++ application/views/Employees/row.php
@@ -62,6 +62,28 @@
                                 </div>
                                 <div class="col-xs-9 text-right">
                                     <div class="huge"></div>
+                                    <div>Payments</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="<?php echo base_url(); ?>employees/payments">
+                            <div class="panel-footer">
+                                <span class="pull-left">Employee Payments</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-comments fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge"></div>
                                     <div>Add</div>
                                 </div>
                             </div>
