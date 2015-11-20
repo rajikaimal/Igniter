f459f6b4227df84bd86a81ca60d3c82de6bc73d5
Rajika <inbox.rajika@gmail.com>
2015-11-20 21:30:31 +0530

    Stock update

--- application/controllers/Stocks.php
+++ application/controllers/Stocks.php
@@ -2,20 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Stocks extends CI_Controller {
-	public function __construct(){
-		parent::__construct();
-	}
-	public function index() {	
-		if(!isset($_SESSION['loggedIn'])){
-            redirect('login','refresh');    
-        }
-		$this->load->view('header');
-		$this->load->view('stocks/row');
-    $this->load->view('stocks/index');
-		$this->load->view('footer');
-		//$this->load->view('footer');
-	}
-	public function readings() {
+  	public function __construct(){
+  		parent::__construct();
+  	}
+  	public function index() {	
+  		if(!isset($_SESSION['loggedIn'])){
+              redirect('login','refresh');    
+          }
+  		$this->load->view('header');
+  		$this->load->view('stocks/row');
+      $this->load->view('stocks/index');
+  		$this->load->view('footer');
+  		//$this->load->view('footer');
+  	}
+  	public function readings() {
         $petrol = $_POST['petrol'];
     	$diesel = $_POST['diesel'];
 
@@ -67,7 +67,10 @@ class Stocks extends CI_Controller {
       $this->load->view('footer');
     }
     public function newLubricantStocks() {
-
+      $this->load->view('header');
+      $this->load->view('stocks/row');
+      $this->load->view('stocks/viewlubricants');
+      $this->load->view('footer');  
     }
     public function lubricants() {
       $this->load->view('header');
