372eefb67c3dec32a6f4836e7079a8a0bff139f7
Rajika <inbox.rajika@gmail.com>
2015-11-26 18:49:47 +0530

    Gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,10 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+	,uglifiy = require('gulp-uglify');
 
+gulp.task('minify',function() {
+	gulp.src('js/**/*.js')
+	.pipe(uglifiy())
+	.pipe(dest('/js/main.min.js'));
+});
+
+gulp.task('default',['minify']);
